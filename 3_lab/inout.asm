45d40f243b8f37f5451a179d2c98eeefb16b04ce
reebuker <spontofs@gmail.com>
2025-12-05 22:56:58 +0300

    added input func

--- 3_lab/inout.asm
+++ 3_lab/inout.asm
@@ -3,6 +3,7 @@ section .data
 section .text
 	global strlen
 	global println
+	global get_input
 
 strlen:
 ; RDI - строка на вход, заканчивается 0
@@ -42,3 +43,40 @@ pop rdx
 pop rax
 
 ret
+
+get_input:
+    ; 1. Читаем с клавиатуры
+    mov rax, 0          ; sys_read = 0
+    mov rdx, rsi        ; размер буфера
+    mov rsi, rdi        ; адрес буфера
+    mov rdi, 0          ; stdin = 0
+    syscall
+    
+    ; 2. Проверяем ошибку
+    test rax, rax
+    js .error           ; если < 0
+    
+    ; 3. Заменяем \n на 0
+    cmp rax, 0
+    je .done            ; пустая строка
+    
+    dec rax                    ; последний символ
+    cmp byte [rsi + rax], 10   ; 10 = \n
+    jne .no_newline
+    
+    mov byte [rsi + rax], 0    ; заменяем \n на 0
+    ; rax уже = длина без \n
+    ret
+
+.no_newline:
+    ; Если \n нет - добавляем 0 в конец
+    inc rax                    ; восстанавливаем длину
+    mov byte [rsi + rax], 0    ; ставим 0
+    
+.done:
+    ret
+
+.error:
+    mov rax, -1
+    ret
+
--- 3_lab/main.asm
+++ 3_lab/main.asm
@@ -1,13 +1,12 @@
 section .data
+
 	enter_type db 'Размерность чисел? знак. / без знак. (i / u):', 10
-	enter_type_len equ $ - enter_type
 
 	enter_a db 'Введите a:'
-	enter_a_len equ $ - enter_a
 
-;	enter_b db 'Введите b:'
+	enter_b db 'Введите b:'
 
-;	program_calc db 'Программа посчитала:'
+	program_calc db 'Программа посчитала:'
 
 ;	var_more db 'a > b ==>'
 
@@ -32,17 +31,26 @@ section .bss
 	global resasm
 	resasm 	resq 1
 
+; резервируем 64 байта для буфера
+	global buffer
+	buffer resb 64
+
 section .text
 ; передаем функии из файла func
 	extern asmfunc_16 
 	extern asmfunc_u16
 	extern strlen
 	extern println
+	extern get_input	
 	global _start
 
 _start:
 	mov rdi, enter_type;
 	call println
+	
+	mov rsi, 63		; размер буфера (+1 для нуля)
+	mov rdi, buffer		; адрес буфера
+	call get_input	
 
 	mov rax, 60
 	xor rdi, rdi
